dab9a4068fed2a31e81b6905c9bcabaea0fc28ad
Rob <rlainewyllie@gmail.com>
2018-05-10 23:43:10 +1000

    remove test unit tests

--- test/system/news_test.rb
+++
@@ -1,9 +0,0 @@
-require "application_system_test_case"
-
-class NewsTest < ApplicationSystemTestCase
-  # test "visiting the index" do
-  #   visit news_url
-  #
-  #   assert_selector "h1", text: "News"
-  # end
-end
