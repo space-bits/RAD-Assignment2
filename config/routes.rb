7cb2aaef95a5f9176e73e0161312258ae94cb499
Rob <rlainewyllie@gmail.com>
2018-05-10 23:52:58 +1000

    Remove the comments scaffold and add test cases

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,4 @@
 Rails.application.routes.draw do
-  resources :comments
   resources :users
   resources :news
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
