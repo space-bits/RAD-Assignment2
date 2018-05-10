531b9f7980a517bbaec650cad66b37c7cd1a3b77
Rob <rlainewyllie@gmail.com>
2018-05-10 23:38:20 +1000

    Add validations to the tests for users

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -4,4 +4,16 @@ class UserTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+
+  test 'username should be atleast 2 characters in length' do
+    # assert
+  end
+
+  test 'username should be at most 15 characters in length' do
+    #
+  end
+
+  test '' do
+    #
+  end
 end
