b9c9994df709b132cf037be6369f146c722e2af4
Rob <rlainewyllie@gmail.com>
2018-05-11 15:51:30 +1000

    Add test cases for the user's username

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,31 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
+  it 'should not have spaces' do
 
+    user.valid?
+    expect(user.errors.messages[]).to include ""
+  end
+
+  it 'should begin with a letter' do
+
+    user.valid?
+    expect(user.errors.messages[]).to include ""
+  end
+
+  it 'should only contain english alphanumeric characters' do
+    user = User.new({"username"=>"abc$","password"=>"password"})
+    user.valid?
+    expect(user.errors.messages[]).to include ""
+  end
+
+  it 'should be no longer than 15 characters' do
+
+  end
+
+  it 'should be no shorter than 2 caharacters' do
+    user = User.new({"username"=>"","password"=>"password"})
+    user.valid?
+    expect(user.errors.messages[]).to include ""
+  end
 end
