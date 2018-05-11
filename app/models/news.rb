a9fd3b791505e10c8b6d620fde9083545bf680fb
Rob <rlainewyllie@gmail.com>
2018-05-11 11:55:22 +1000

    Comment out the by field for a new news article

--- app/models/news.rb
+++ app/models/news.rb
@@ -4,5 +4,5 @@ class News < ApplicationRecord
   has_many :comments, dependent: :destroy
 
   # validate that the fields must match regex
-  validates :body, :by, presence: true
+  validates :body, :url, :title, presence: true
 end
