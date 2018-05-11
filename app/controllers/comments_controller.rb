c7b59bb17b8c0761a6f47919eb48ff04ac47a995
Rob <rlainewyllie@gmail.com>
2018-05-11 12:37:51 +1000

    Add methods for adding comments to posts to the controller

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,2 +1,17 @@
 class CommentsController < ApplicationController
+  # TODO: Link a comment to a user id that posted it
+  def create
+    # when creating a new comment, attach it to a post id and
+    # redirect to the post
+    @post = Post.find(params[:post_id])
+    @comment = @post.comments.create!(comment_params)
+    redirect_to @post
+  end
+
+  # decalre a new private method to allow that a comment requires a body
+  private
+
+  def comment_params
+    params.require(:comment).permit(:body)
+  end
 end
