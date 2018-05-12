cd4c2cf3319a25f94c4feb4b19578f3b8eaf3b3f
Rob <rlainewyllie@gmail.com>
2018-05-12 20:44:16 +1000

    Add test spec for comments

--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -1,5 +1,17 @@
 require 'rails_helper'
 
 RSpec.describe Comment, type: :model do
+  describe 'text' do
+    it 'should not be blank' do
+      comment = Comment.new({"news_id"=>"", "comment"=>""})
+    end
 
+    it 'should have more than 3 characters not including white space' do
+
+    end
+
+    it 'should be less than 1000 characters' do
+
+    end
+  end
 end
