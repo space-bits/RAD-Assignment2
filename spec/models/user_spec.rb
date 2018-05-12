9373003290e29fc7b30cccd9cd2efa3fc52a9142
Rob <rlainewyllie@gmail.com>
2018-05-12 20:06:37 +1000

    Add test cases for passwords

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -39,9 +39,31 @@ RSpec.describe User, type: :model do
       expect(user.errors.messages[:username]).to include "Username already taken."
     end
   end
-  describe 'password' do
-    it 'should be longer than 10 characters' do
 
+  # test cases for passwords
+  describe 'password' do
+    it 'should be at least 10 characters' do
+      user = User.new({"username"=>"abc","password"=>"Aa!345678"})
+      user.valid?
+      expect(user.errors.messages[:password]).to include "Password must contain at least 1 uppercase letter, 1 lowercase letter, 1 special character, and 1 number."
+    end
+
+    it 'should contain at least 1 special characters' do
+      user = User.new({"username"=>"abc","password"=>"Aa2345678"})
+      user.valid?
+      expect(user.errors.messages[:password]).to include "Password must contain at least 1 uppercase letter, 1 lowercase letter, 1 special character, and 1 number."
+    end
+
+    it 'should contain at least 1 uppercase letter' do
+      user = User.new({"username"=>"abc","password"=>"0a!345678"})
+      user.valid?
+      expect(user.errors.messages[:password]).to include "Password must contain at least 1 uppercase letter, 1 lowercase letter, 1 special character, and 1 number."
+    end
+
+    it 'should contain at least 1 lowercase letter' do
+      user = User.new({"username"=>"abc","password"=>"A1!345678"})
+      user.valid?
+      expect(user.errors.messages[:password]).to include "Password must contain at least 1 uppercase letter, 1 lowercase letter, 1 special character, and 1 number."
     end
   end
 end
