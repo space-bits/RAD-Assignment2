d88ccb8d40470587444c27333b350814cb1b9ff6
Rob <rlainewyllie@gmail.com>
2018-05-12 19:54:53 +1000

    Add rmit logo and test cases for user

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,25 +1,32 @@
 class User < ApplicationRecord
-  has_secure_password
-
   has_many :comments, dependent: :destroy
 
   # validate the username
-  validates :username, presence: true, length: {
+  validates :username, length: {
     maximum: 25,
-      too_long: "must be between 3 and 25 characters"
+      too_long: "must be between 3 and 25 characters."
     }, length: {
     minimum: 3,
-      too_short: "must be between 3 and 25 characters"
-  }
+      too_short: "must be between 3 and 25 characters."
+  },
+  uniqueness: { case_sensitive: false }
 
+  # alphanumeric, and special characters and
   # validate the password
-  validates :password, presence: true, length: {
+  validates :password, length: {
     maximum: 25,
-      too_long: "must be between 3 and 25 characters"
+      too_long: "must be between 3 and 25 characters."
     }, length: {
     minimum: 3,
-      too_short: "must be between 3 and 25 characters"
+      too_short: "must be between 3 and 25 characters."
+  },
+  format: {
+    with: /[A-Za-z\s\d\!\#\$%\&\'*\+-.^_\`|~:]/,
+      message: "must have one number, letter, capital letter, and special
+        character (ie. !, @, #). May contain spaces."
   }
 
   validates :password_confirmation, presence: true
+  
+  has_secure_password
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,31 +1,47 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  it 'should not have spaces' do
+  describe 'username' do
+    it 'should not have spaces' do
+      user = User.new({"username"=>"ab c$","password"=>"password"})
+      user.valid?
+      expect(user.errors.messages[:username]).to include "Username cannot contain spaces."
+    end
 
-    user.valid?
-    expect(user.errors.messages[]).to include ""
+    it 'should begin with a letter' do
+      user = User.new({"username"=>"3abc$","password"=>"password"})
+      user.valid?
+      expect(user.errors.messages[:username]).to include "Username must begin with a letter."
+    end
+
+    it 'should only contain alphanumeric characters' do
+      user = User.new({"username"=>"abc$","password"=>"password"})
+      user.valid?
+      expect(user.errors.messages[:username]).to include "Username can only contain letters, and numbers."
+    end
+
+    it 'should be shorter than 15 characters' do
+      user = User.new({"username"=>"abcdefghijklmnop","password"=>"password"})
+      user.valid?
+      expect(user.errors.messages[:username]).to include "Username must be between 2 and 15 characters."
+    end
+
+    it 'should longer than 2 characters' do
+      user = User.new({"username"=>"a","password"=>"password"})
+      user.valid?
+      expect(user.errors.messages[:username]).to include "Username must be between 2 and 15 characters."
+    end
+
+    it 'should be unique' do
+      user = User.new({"username"=>"a","password"=>"password"})
+      user2 = User.new({"username"=>"a","password"=>"password"})
+      user2.valid?
+      expect(user.errors.messages[:username]).to include "Username already taken."
+    end
   end
+  describe 'password' do
+    it 'should be longer than 10 characters' do
 
-  it 'should begin with a letter' do
-
-    user.valid?
-    expect(user.errors.messages[]).to include ""
-  end
-
-  it 'should only contain english alphanumeric characters' do
-    user = User.new({"username"=>"abc$","password"=>"password"})
-    user.valid?
-    expect(user.errors.messages[]).to include ""
-  end
-
-  it 'should be no longer than 15 characters' do
-
-  end
-
-  it 'should be no shorter than 2 caharacters' do
-    user = User.new({"username"=>"","password"=>"password"})
-    user.valid?
-    expect(user.errors.messages[]).to include ""
+    end
   end
 end
