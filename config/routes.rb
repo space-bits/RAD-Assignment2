9eb2e2c7d077d90a080e270fb78e352b5e1e487d
Rob <rlainewyllie@gmail.com>
2018-05-13 17:38:09 +1000

    Fix navbar to route properly according to routes deifned in routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
   root 'news#index'
 
-  get '/signup' to: 'users#new'
-  get '/login' to: 'sessions#new'
-  get '/logout' to: 'sessions#destroy'
+  get '/signup', to: 'users#new'
+  get '/login', to: 'sessions#new'
+  get '/logout', to: 'sessions#destroy'
 
   resources :users
   resources :news do
