1265046ffbf34d2584f4d949c8060ce43bb392f3
Rob <rlainewyllie@gmail.com>
2018-05-13 18:40:41 +1000

    Creatie helper methods related to user sign in

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,18 +1,16 @@
 class SessionsController < ApplicationController
   def new
+
   end
 
   def create
     user = User.find_by(username: params[:session][:username])
     if user && user.authenticate(params[:session][:password])
       # Log the user in and redirect to the user's show page.
-      redirect_to(@user)
+      log_in user
+      redirect_to user
     else
-      # Create an error message.
-      respond_to do |format|
-        format.html { redirect_to login_url, notice: 'Login failed.' }
-        format.json { head :no_content }
-      end
+      # generate an error message
     end
   end
 
@@ -20,8 +18,4 @@ class SessionsController < ApplicationController
 
   end
 
-  private
-    def logged_in?
-
-    end
 end
--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -2,4 +2,14 @@ module SessionsHelper
   def login(user)
     session[:user_id] = user.id
   end
+
+  def current_user
+    # assign the current user if one exists in the database matching the username stored in the session
+    @current_user ||= User.find_by(username: session[:username])
+  end
+
+  # if the current user is set then return true
+  def logged_in?
+    !current_user.nil?
+  end
 end
