c6e8eeee52c742df34dcb012ab87c58982fcc1ad
Rob <rlainewyllie@gmail.com>
2018-05-13 14:18:48 +1000

    Add the prof_pic method and display image if theres a saved img

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,2 +1,16 @@
 module UsersHelper
+
+  # adapted from Micheal Hartl, https://www.railstutorial.org/book/sign_up
+  # image for a given user. Render this from the user.grav_url from the db
+  def prof_pic_for(user)
+    # we don't need an id for the user's grav url.
+    # prof_pic_id = Digest::MD5::hexdigest(user.username)
+    if user.grav_url.nil? or user.grav_url.empty?
+      image_tag("", alt: user.username, class: "gravatar")
+
+    else
+      prof_pic_url = user.grav_url
+      image_tag(prof_pic_url, alt: user.username, class: "gravatar")
+    end
+  end
 end
