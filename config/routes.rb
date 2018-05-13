b4a4fa3dba1d92e8751d207054e70a8a91a84df1
Rob <rlainewyllie@gmail.com>
2018-05-13 17:31:15 +1000

    Map login signup and logout routes to sessions and users

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,13 @@
 Rails.application.routes.draw do
-  get 'sessions/new'
   root 'news#index'
 
-  resources :users
+  get '/signup' to: 'users#new'
+  get '/login' to: 'sessions#new'
+  get '/logout' to: 'sessions#destroy'
 
+  resources :users
   resources :news do
     resources :comments, only: [:create]
   end
-
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
