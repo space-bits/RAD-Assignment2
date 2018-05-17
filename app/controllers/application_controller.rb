683908760427fbedd33da1a672bc81a2ab6d435c
Rob <rlainewyllie@gmail.com>
2018-05-17 12:13:38 +1000

    Add require login helper method

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,5 +2,14 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   include SessionsHelper
 
-  # before_action :authenticate!
+  before_action :require_login
+
+  private
+
+   def require_login
+     unless logged_in?
+       flash[:error] = "You must be logged in to access this section"
+       redirect_to login_url # halts request cycle
+     end
+   end
 end
--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -18,5 +18,12 @@ module SessionsHelper
     session.delete(:user_id)
     @current_user = nil
   end
+  
+  def require_login
+   unless logged_in?
+     flash[:error] = "You must be logged in to access this section"
+     redirect_to login_url # halts request cycle
+   end
+  end
 
 end
