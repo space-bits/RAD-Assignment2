87c04800796aa292e5d5d1a55a4b963c0a6d2216
Rob <rlainewyllie@gmail.com>
2018-05-18 21:51:43 +1000

    Change back to logged_in? for header

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -42,10 +42,10 @@ module SessionsHelper
   end
 
   def require_login
-   unless logged_in?
-     flash[:error] = "You must be logged in to access this section"
-     redirect_to login_url # halts request cycle
-   end
+    unless logged_in?
+      flash[:error] = "You must be logged in to access this section"
+      redirect_to login_url # halts request cycle
+    end
   end
 
 end
