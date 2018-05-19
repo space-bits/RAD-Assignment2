dd9140bf0c7116e518f908af0e9ff7522b55f413
Rob <rlainewyllie@gmail.com>
2018-05-19 13:39:46 +1000

    Remove id from comment spec

--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -1,27 +1,36 @@
 require 'rails_helper'
 
 RSpec.describe Comment, type: :model do
+
+  def setup
+     @comment = Comment.new(article_id: 1, body: "Abcdefasdj", user_id: 1)
+   end
+
+   test 'should be valid' do
+     assert @comment.valid?
+   end
+
   describe 'body' do
     it 'should not be nil' do
-      comment = Comment.new({"id"=>1, "article_id"=>1, "user_id"=>1})
+      comment = Comment.new({"article_id"=>1, "user_id"=>1})
       comment.valid?
       expect(comment.messages.errors[:comment]).to include "Message can't be blank"
     end
 
     it 'should not be empty' do
-      comment = Comment.new({"id"=>1, "article_id"=>1, "body"=>"", "user_id"=>1})
+      comment = Comment.new({"article_id"=>1, "body"=>"", "user_id"=>1})
       comment.valid?
       expect(comment.messages.errors[:comment]).to include "Message can't be blank"
     end
 
     it 'should have more than 3 characters not including white space' do
-      comment = Comment.new({"id"=>1, "article_id"=>1, "body"=>"a aa", "user_id"=>1})
+      comment = Comment.new({"article_id"=>1, "body"=>"a aa", "user_id"=>1})
       comment.valid?
       expect(comment.messages.errors[:comment]).to include "Message can't be blank"
     end
 
     it 'should be less than 1000 characters' do
-      comment = Comment.new({"id"=>1, "article_id"=>1, "body"=>"a" * 1001, "user_id"=>1})
+      comment = Comment.new({"article_id"=>1, "body"=>"a" * 1001, "user_id"=>1})
       comment.valid?
       expect(comment.messages.errors[:comment]).to include "Message can't be blank"
     end
@@ -29,13 +38,13 @@ RSpec.describe Comment, type: :model do
 
   describe 'article_id' do
     it 'should not be nil' do
-      comment = Comment.new({"id"=>1, "body"=>"abcddef", "user_id"=>1})
+      comment = Comment.new({"body"=>"abcddef", "user_id"=>1})
       comment.valid?
       expect(comment.messages.errors[:comment]).to include "Message must belong to an article"
     end
 
     it 'should exist in the db' do
-      comment = Comment.new({"id"=>1, "article_id"=>500, "body"=>"a" * 1001, "user_id"=>1})
+      comment = Comment.new({"article_id"=>500, "body"=>"abcasd", "user_id"=>1})
       comment.valid?
       expect(comment.messages.errors[:comment]).to include "Message must belong to an article"
     end
@@ -43,13 +52,13 @@ RSpec.describe Comment, type: :model do
 
   describe 'user_id' do
     it 'should not be nil' do
-      comment = Comment.new({"id"=>1, "article_id"=>2, "body"=>"abcddef"})
+      comment = Comment.new({"article_id"=>2, "body"=>"abcddef"})
       comment.valid?
       expect(comment.messages.errors[:comment]).to include "Message must belong to a user"
     end
 
     it 'should exist in the db' do
-      comment = Comment.new({"id"=>1, "article_id"=>2, "body"=>"abcdef", "user_id"=>102222200})
+      comment = Comment.new({"article_id"=>2, "body"=>"abcdef", "user_id"=>102222200})
       comment.valid?
       expect(comment.messages.errors[:comment]).to include "Message must belong to a user"
     end
