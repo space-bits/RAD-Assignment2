0374fdd465667e7e9912134224b53794b425d453
Rob <rlainewyllie@gmail.com>
2018-05-20 23:47:56 +1000

    Finish writing comment validations/tests

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,8 +1,22 @@
 class Comment < ApplicationRecord
   belongs_to :article
 
+  VALID_BODY_REGEX = /\s*\S\s*\S\s*\S.*/
   validates :body,
-    presence: true,
-    length: {in: 3..1000},
-    format: {with: /\s*\S\s*\S\s*\S.*/}
+    presence: {
+      message: "can't be blank"
+    },
+    length: {
+      in: 3..1000,
+        message: "must be between 3 and 1000 characters"
+    },
+    format: {
+      with: VALID_BODY_REGEX
+    }
+
+  validates :user_id,
+    presence: true
+
+  validates :article_id,
+    presence: true
 end
