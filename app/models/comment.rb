e2aeae2a8a5eebf08c32d03c192a15e18aa55a78
Fauxtography <gabrielhaines@hotmail.com>
2018-05-20 14:54:08 +1000

    removed text field from articles and validated comments

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,5 +2,7 @@ class Comment < ApplicationRecord
   belongs_to :article
 
   validates :body,
-    presence: true
+    presence: true,
+    length: {in: 3..1000},
+    format: {with: /\s*\S\s*\S\s*\S.*/}
 end
