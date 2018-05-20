fe45c81a04d2a517474498c2cb397169256e33ee
Rob <rlainewyllie@gmail.com>
2018-05-20 23:15:39 +1000

    Fix regex to properly validate uname and pass

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -21,7 +21,7 @@ RSpec.describe User, type: :model do
     end
 
     it 'should at most 15 characters' do
-      user = User.new({"username"=>"abcdefghijklmnop","password"=>"password", "password_confirmation"=>"password"})
+      user = User.new({"username"=>"a" * 16,"password"=>"password", "password_confirmation"=>"password"})
       user.valid?
       expect(user.errors.messages[:username]).to include "must be between 2 and 15 characters"
     end
@@ -33,37 +33,39 @@ RSpec.describe User, type: :model do
     end
 
     it 'should be unique' do
-      user = User.new({"username"=>"a","password"=>"password", "password_confirmation"=>"password"})
-      user2 = User.new({"username"=>"a","password"=>"password", "password_confirmation"=>"password"})
+      user = User.new({"username"=>"abc","password"=>"password", "password_confirmation"=>"password"})
+      user2 = User.new({"username"=>"abc","password"=>"password", "password_confirmation"=>"password"})
+      user.save
       user2.valid?
-      expect(user.errors.messages[:username]).to include "must be a unique username in the database"
+      expect(user.errors.messages[:username]).to include "has already been taken"
+      user.destroy
     end
   end
 
   # test cases for passwords
   describe 'password' do
     it 'should be at least 10 characters' do
-      user = User.new({"username"=>"abc","password"=>"Aa!345678","password_confirmation"=>"Aa!345678"})
+      user = User.new({"username"=>"abc", "password"=>"a" * 9, "password_confirmation"=>"a" * 9})
       user.valid?
-      expect(user.errors.messages[:password]).to include "Password must be at least 10 characters"
+      expect(user.errors.messages[:password]).to include "must be at least 10 characters"
     end
 
     it 'should contain at least 1 special characters' do
-      user = User.new({"username"=>"abc","password"=>"Aa2345678000","password_confirmation"=>"Aa2345678000"})
+      user = User.new({"username"=>"abc","password"=>"Aa2345678000", "password_confirmation"=>"Aa2345678000"})
       user.valid?
-      expect(user.errors.messages[:password]).to include "Password must contain at least 1 uppercase letter, 1 lowercase letter, 1 special character, and 1 number"
+      expect(user.errors.messages[:password]).to include "must contain at least 1 uppercase letter, 1 lowercase letter, 1 special character, and 1 number"
     end
 
     it 'should contain at least 1 uppercase letter' do
-      user = User.new({"username"=>"abc","password"=>"0a!345678000","password_confirmation"=>"0a!345678000"})
+      user = User.new({"username"=>"abc","password"=>"0a!345678000", "password_confirmation"=>"0a!345678000"})
       user.valid?
-      expect(user.errors.messages[:password]).to include "Password must contain at least 1 uppercase letter, 1 lowercase letter, 1 special character, and 1 number"
+      expect(user.errors.messages[:password]).to include "must contain at least 1 uppercase letter, 1 lowercase letter, 1 special character, and 1 number"
     end
 
     it 'should contain at least 1 lowercase letter' do
-      user = User.new({"username"=>"abc","password"=>"A1!345678000","password_confirmation"=>"A1!345678000"})
+      user = User.new({"username"=>"abc","password"=>"A1!345678000", "password_confirmation"=>"A1!345678000"})
       user.valid?
-      expect(user.errors.messages[:password]).to include "Password must contain at least 1 uppercase letter, 1 lowercase letter, 1 special character, and 1 number"
+      expect(user.errors.messages[:password]).to include "must contain at least 1 uppercase letter, 1 lowercase letter, 1 special character, and 1 number"
     end
   end
 end
