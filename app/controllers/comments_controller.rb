7a2d6fb30e45506e2561261af2453e39e6b4f141
Rob <rlainewyllie@gmail.com>
2018-05-21 00:17:05 +1000

    Add validation for the presence of user id and article id

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,7 +1,6 @@
 class CommentsController < ApplicationController
   # don't require login to view comments
   skip_before_action :require_login, only: [:show, :show_top_20]
-  # TODO: Link a comment to a user id that posted it
 
   # when creating a new comment, attach it to a articles id and
   # redirect to the articles article
@@ -14,7 +13,7 @@ class CommentsController < ApplicationController
       # format.json { render :show, status: :created, location: @article }
       redirect_to @article
     else
-      format.html { render :show }
+      format.html { redirect_to @article }
       format.json { render json: @article.errors, status: :unprocessable_entity }
     end
   end
