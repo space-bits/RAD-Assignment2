a1e0970ed218b6ffe6fc0253cd608665d520cd43
Rob <rlainewyllie@gmail.com>
2018-05-20 23:32:58 +1000

    Make all of the article tests work

--- app/models/article.rb
+++ app/models/article.rb
@@ -7,11 +7,12 @@ class Article < ApplicationRecord
   # TODO: add validation
   validates_format_of :url,
     :with => URI::regexp(%w(http https)),
-    presence: true
-
-
+      message: "must be a valid url"
 
   validates :title,
-    length: {in: 10..199},
+    length: {
+      in: 10..199,
+        message: "must be between 10 and 200 characters"
+    },
     presence: true
 end
