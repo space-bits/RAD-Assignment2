faf3ac1f2e422367b40bf645c3f4e68294b8b288
Rob <rlainewyllie@gmail.com>
2018-05-24 17:19:38 +1000

    Add merge with type for the hash that is rendered as json

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -3,10 +3,12 @@ class ItemsController < ApplicationController
 
   def show
     if Article.exists?(params[:id])
+      @type = "news"
       render json: Article.find(params[:id])
     elsif Comment.exists?(params[:id])
       # ['article_id LIKE ?', "%#{params[:id]}%"]
       # possibly we have to rely on the article_id
+      @type = "comment"
       render json: Comment.find(params[:id])
     else
       render json: "{ \"errors\" : {\"message\" : \"Item doesn\'t exist\"} }"
